30f88966c20a59e8a59eb0cac7197bee49a642a8
DeepaRoy66 <159435065+DeepaRoy66@users.noreply.github.com>
2025-11-06 22:09:12 +0545

    Remove Popup component and its usage from Landing page for codebase cleanup

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -2,7 +2,6 @@
 
 import Hero from "./Hero";
 import Introduction from "./Introduction";
-import Popup from "./PopUp";
 import Programs from "./Programs";
 
 
@@ -12,7 +11,6 @@ import WhyUs from "./WhyUs";
 export default function Landing(){
 return(
   <div className="overflow-x-hidden">
-  <Popup/>
   <Hero/>
   <Introduction/>
   <WhyUs/>
